b4ef8b2f32de30d603b6874db98fe075f77cf7fe
pedroT <pedrotavares41@gmail.com>
2017-03-22 18:32:13 +0000

    Angularjs 2 Example

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-
+import {SpotifyService} from './services/spotify.service';
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app.component.html',
+  providers: [SpotifyService]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {  }
